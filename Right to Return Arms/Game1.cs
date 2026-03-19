e6030c0d6b135641c14f3cc28a2d899481d878b1
Etdd417 <etdd522@gmail.com>
2026-03-19 16:07:39 -0400

    Began implementing into Game
    Started using Button in game, and prepared to merge back into the main branch

--- Right to Return Arms/Game1.cs
+++ Right to Return Arms/Game1.cs
@@ -7,10 +7,48 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Right_to_Return_Arms
 {
+    // Game state enum
+
+    public enum GameState
+    {
+        Title,
+        Pause,
+        ItemPickup,
+        GameOver,
+        HighScores,
+        Game
+    }
+    
+    
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        // Keeping track of Finite state machine
+        private GameState _gameState;
+        // Previous mouse state
+        private MouseState _previousMouseState;
+
+        // Temporary UI variables that can be moved to manager later
+        
+        // Title Screen vars
+        Button _startBut;
+        Button _closeGameBut;
+        Button _highscoresBut;
+
+        // Pause Screen vars
+        Button _resumeBut;
+        Button _menuBut; // Menu Button will likely be reused;
+
+        // Game Over vars
+        // Will have the return to menu button
+        // Will have the highscores button
+        // Retry button at some point??
+
+        // High Scores vars
+        // Will have the menu button
+
+        // End of temporary variables
 
         public Game1()
         {
@@ -30,7 +68,7 @@ namespace Right_to_Return_Arms
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -38,7 +76,36 @@ namespace Right_to_Return_Arms
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Finite state machine, can be moved to manager later
+            MouseState ms = Mouse.GetState();
+            switch (_gameState)
+            {
+                case GameState.Title:
+                    if (_startBut.mouseIntersects(ms) && SingleMouseClick())
+                    {
+                        _gameState = GameState.Game;
+                    }
+                    else if (_closeGameBut.mouseIntersects(ms) && SingleMouseClick())
+                    {
+                        Exit();
+                    }
+                        break;
+                case GameState.Pause:
+
+                    break;
+                case GameState.ItemPickup:
+
+                    break;
+                case GameState.GameOver:
+
+                    break;
+                case GameState.HighScores:
+
+                    break;
+                case GameState.Game:
+
+                    break;
+            }
 
             base.Update(gameTime);
         }
@@ -47,8 +114,22 @@ namespace Right_to_Return_Arms
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            _spriteBatch.Begin();
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+        /// <summary>
+        /// Helper method to check if the mouse has been pressed for a single turn
+        /// </summary>
+        /// <returns></returns>
+        private bool SingleMouseClick()
+        {
+            return (Mouse.GetState().LeftButton == ButtonState.Pressed) && 
+                _previousMouseState.LeftButton == ButtonState.Released;
+        }
+
     }
 }
