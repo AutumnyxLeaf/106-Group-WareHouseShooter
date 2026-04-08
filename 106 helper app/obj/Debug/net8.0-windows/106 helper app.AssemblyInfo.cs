aa534fc3541f9654c43ad38d5f394317c9620836
Fluffynimbus <ixmanmd07@gmail.com>
2026-04-08 16:13:43 -0400

    not final do not touch

--- 106 helper app/obj/Debug/net8.0-windows/106 helper app.AssemblyInfo.cs
+++ 106 helper app/obj/Debug/net8.0-windows/106 helper app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("106 helper app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+981a54eb85e7f92320dd9cbf56222a80476fcb43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d57f4e3cb49021a2f20184aeb8bceecaa5b1aba")]
 [assembly: System.Reflection.AssemblyProductAttribute("106 helper app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("106 helper app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
